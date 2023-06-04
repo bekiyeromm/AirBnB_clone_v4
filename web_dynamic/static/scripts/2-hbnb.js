04d08abe607e5597f18a4928c1c1d629cb97dddc
bekiyeromm <bk3tena@gmail.com>
2023-06-04 02:11:34 -0700

    Before requesting the HBNB API, it’s better to know the status of this one.
    Update the API entry point (api/v1/app.py) by replacing the current CORS CORS(app, origins=0.0.0.0) by CORS(app, resources={r/api/v1/*: {origins: *}}).
    
    Change the route 1-hbnb to 2-hbnb in the file 2-hbnb.py (based on 1-hbnb.py)
    
    Create a new template 2-hbnb.html (based on 1-hbnb.html) and update it:
    
    Import the JavaScript static/scripts/2-hbnb.js in the <head> tag (instead of 1-hbnb.js)
    Add a new div element in the header tag:
    Attribute ID should be api_status
    Align to the right
    Circle of 40px diameter
    Center vertically
    At 30px of the right border
    Background color #cccccc
    Also add a class available for this new element in web_dynamic/static/styles/3-header.css:
    Background color #ff545f
    Write a JavaScript script (static/scripts/2-hbnb.js):
    
    Based on 1-hbnb.js
    Request http://0.0.0.0:5001/api/v1/status/:
    If in the status is “OK”, add the class available to the div#api_status
    Otherwise, remove the class available to the div#api_status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
